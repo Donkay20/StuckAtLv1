5404dcc520e79fa030c1197c7c390ab4e922fbbe
Melocalyptic <philip.kim830@gmail.com>
2023-10-16 21:17:30 -0400

    PlayerAbnim ation

--- StuckAtLv1/Assets/Movement.cs
+++ StuckAtLv1/Assets/Movement.cs
@@ -8,9 +8,17 @@ public class Movement : MonoBehaviour
 {
     Rigidbody2D body;
     Vector3 movementVector;
+    Animator anim;
+    SpriteRenderer sr;
+
     [SerializeField] float speed = 3f;
+
+
     private void Awake() {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
         movementVector = new Vector3();
     }
 
@@ -20,5 +28,28 @@ public class Movement : MonoBehaviour
         movementVector.y = Input.GetAxisRaw("Vertical");
         movementVector *= speed;
         body.velocity = movementVector;
+
+        RunAnimation();
+    }
+
+    void RunAnimation()
+    {
+        if(movementVector.x != 0 || movementVector.y != 0)
+        {
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+
+        if(movementVector.x > 0)
+        {
+            sr.flipX = false;
+        }
+        else if(movementVector.x < 0)
+        {
+            sr.flipX = true;
+        }
     }
 }
