267130e75aa18d07d79c71dd847f8871f48512f1
Melocalyptic <philip.kim830@gmail.com>
2023-11-10 17:48:04 -0500

    Added rock art, script not implemented yet

---
+++ StuckAtLv1/Assets/Scripts/Attacks/RockThrow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockThrow : MonoBehaviour
+{
+    private float timer = 2f;
+    private float speed = 1f;
+    private Rigidbody2D rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        
+    }
+}
