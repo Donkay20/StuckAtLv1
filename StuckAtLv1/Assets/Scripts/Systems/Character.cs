ffe35ff08268536aaa71fd513a63fe07deb2bc5e
msett <supersett@hotmail.com>
2023-11-13 11:03:07 -0500

    Prototype v3.7
    Edited the Character script to implement invincibility frames; main character can't be damaged frame-by-frame anymore

--- StuckAtLv1/Assets/Scripts/Systems/Character.cs
+++ StuckAtLv1/Assets/Scripts/Systems/Character.cs
@@ -1,22 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Profiling;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class Character : MonoBehaviour
+/*
+Handles main character's active stats in combat, their buffs, and their damage handling.
+
+todo: 
+> implement variables and conditions for buffs and debuffs
+> implement temp hp
+*/
 {
     public int maxHp = 10;
     public int currentHp = 10;
+    private readonly float iframe = 0.2f;
+    private bool invincible;
     [SerializeField] StatusBar hpBar;
 
-    public void TakeDamage(int damage) {
-        currentHp -= damage;
+    public void TakeDamage(int damage) { //todo; edit for buffs/dmg reduction values
+        if (!invincible) {
+            invincible = true;
+            currentHp -= damage;
+            StartCoroutine(InvincibilityFrame());
+        }
+        
         if (currentHp <= 0) {
             Debug.Log("Jamp is dead.");
         }
         hpBar.SetState(currentHp, maxHp);
     }
 
-    public void Heal(int amount) {
+    IEnumerator InvincibilityFrame() {              //elapse this amt of time before the character can be hit again
+        yield return new WaitForSeconds(iframe);    //anything that would increase the amt of iframes would prob go here, although idk if this will be a bonus
+        invincible = false;
+    }
+
+    public void Heal(int amount) {  //needs to be changed to account for overhealing / temp HP
         if (currentHp <= 0) {
             return;
         }
