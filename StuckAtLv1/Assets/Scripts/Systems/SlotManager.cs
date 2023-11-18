1c4d3447ec02a7c9e05fd9e7404ca24fe1c86409
Melocalyptic <philip.kim830@gmail.com>
2023-11-18 00:28:02 -0500

    Organized Slot Manager Script to make it more readable

--- StuckAtLv1/Assets/Scripts/Systems/SlotManager.cs
+++ StuckAtLv1/Assets/Scripts/Systems/SlotManager.cs
@@ -19,19 +19,26 @@ public class SlotManager : MonoBehaviour
     [SerializeField] private Sprite[] offSpriteList;
 
     private void Awake() {
+        //Sets the first slot as default
         slots[0].GetComponent<Animator>().SetTrigger("Hit");
-        slots[0].transform.Find("Slot 1_Border").GetComponent<Image>().sprite = onSpriteList[0];
+        slots[0].transform.GetChild(1).GetComponent<Image>().sprite = onSpriteList[0];
         slotNum = 1;
         slot1.Identity = 1;
         slot2.Identity = 2;
     }
     void Update()
     {
+        ToggleSlot();
+        InititateSlot();
+        TurnOffSlots();
+    }
+
+    //Select Slot Player wants to sue
+    private void ToggleSlot()
+    {
         if (Input.GetKeyDown(slotKey1)) {
             slotNum = 1;
-            slots[0].GetComponent<Animator>().ResetTrigger("Hit2");
-            slots[0].GetComponent<Animator>().SetTrigger("Hit");
-            slots[0].transform.Find("Slot 1_Border").GetComponent<Image>().sprite = onSpriteList[0];
+            AnimationControl(slots[0], 0);
 
             //Debug.Log("Slot 1 pressed.");
             //slot1.Engage();
@@ -39,18 +46,14 @@ public class SlotManager : MonoBehaviour
 
         if (Input.GetKeyDown(slotKey2)) {
             slotNum = 2;
-            slots[1].GetComponent<Animator>().ResetTrigger("Hit2");
-            slots[1].GetComponent<Animator>().SetTrigger("Hit");
-            slots[1].transform.Find("Slot 2_Border").GetComponent<Image>().sprite = onSpriteList[1];
+            AnimationControl(slots[1], 1);
             //Debug.Log("Slot 2 pressed.");
             //slot2.Engage();
         }
-
-        PickSlot();
-        TurnOffSlots();
     }
 
-    private void PickSlot()
+    //Initiates slot selected
+    private void InititateSlot()
     {
         if(Input.GetMouseButtonDown(0))
         {
@@ -66,6 +69,13 @@ public class SlotManager : MonoBehaviour
             }
         }
         
+    }
+
+    private void AnimationControl(GameObject slot, int spriteNumOn)
+    {
+        slot.GetComponent<Animator>().ResetTrigger("Hit2");
+        slot.GetComponent<Animator>().SetTrigger("Hit");
+        slot.transform.GetChild(1).GetComponent<Image>().sprite = onSpriteList[spriteNumOn];
     }
 
     private void TurnOffSlots()
