0b463e139e3e858a8db598ce41356de641110137
adasamiee <ada180@my.yorku.ca>
2024-01-10 21:21:58 -0500

    added opening scene dialogue

--- StuckAtLv1/Assets/Scripts/Dialogue/DialogueManager.cs
+++ StuckAtLv1/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -19,6 +19,8 @@ public class DialogueManager : MonoBehaviour
     {
         lineText.text = string.Empty;
         storeLines();
+        displayNextSentence();
+        displayNextName();
     }
 
     // Update is called once per frame
