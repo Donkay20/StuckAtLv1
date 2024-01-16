5e74c6100102962b4fb21b151ce264d2dfb92214
msett <supersett@hotmail.com>
2024-01-16 12:51:16 -0500

    v0.48b - Event Progress
    More event updates

--- StuckAtLv1/Assets/Scripts/Systems/EventManager.cs
+++ StuckAtLv1/Assets/Scripts/Systems/EventManager.cs
@@ -1,27 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class EventManager : MonoBehaviour
 {
-    //temporary, todo
     [SerializeField] private Button confirm;
+    //temporary buttons
     [SerializeField] private GameManager manager;
     [SerializeField] private MapManager map;
+    [SerializeField] private Character player;
     //backend stuff
     [SerializeField] private Event[] ruinsEvents, forestEvents, sewerEvents, abyssEvents;
+
+    [SerializeField] private TextMeshProUGUI title, dialogueText, button1, button2, button3, health, afterimages, money;
+   
     private Event selectedEvent;
-    private int counter;
-    void Awake()
+    private int messageCounter;
+     void Awake()
     {
         InitializeButtons();
     }
 
     public void InitializeEvent() {
+
+        messageCounter = 0;
+        title.text = "";
+        dialogueText.text = "";
+        button1.text = ""; button2.text = ""; button3.text = "";
+        health.text = player.currentHp.ToString();
+        afterimages.text = 0.ToString(); //todo
+        money.text = 0.ToString(); //todo
         
-        //reset the stuff from before I guess
+        //reset the stuff from before
 
         switch(map.GetWorld()) {
             case 1: //ruins
