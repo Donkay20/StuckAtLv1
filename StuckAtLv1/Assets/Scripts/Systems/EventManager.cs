bec6e7691ea0d2989e954c5058de8631e00ba632
Donkay20 <supersett@hotmail.com>
2024-01-16 01:35:52 -0500

    v0.48
    Fixed some issues

--- StuckAtLv1/Assets/Scripts/Systems/EventManager.cs
+++ StuckAtLv1/Assets/Scripts/Systems/EventManager.cs
@@ -2,15 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class EventManager : MonoBehaviour
 {
     //temporary, todo
     [SerializeField] private Button confirm;
     [SerializeField] private GameManager manager;
+    [SerializeField] private MapManager map;
+    //backend stuff
+    [SerializeField] private Event[] ruinsEvents, forestEvents, sewerEvents, abyssEvents;
+    private Event selectedEvent;
+    private int counter;
     void Awake()
     {
         InitializeButtons();
+    }
+
+    public void InitializeEvent() {
+        
+        //reset the stuff from before I guess
+
+        switch(map.GetWorld()) {
+            case 1: //ruins
+                selectedEvent = ruinsEvents[Random.Range(0, ruinsEvents.Length)];
+                break;
+            case 2: //forest
+                //todo
+                break;
+            case 3: //sewer
+                //todo
+                break;
+            case 4: //abyss
+                //todo
+                break;
+        }
     }
 
     private void Exit() {
