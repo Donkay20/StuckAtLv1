aa11c6b5dd033cdc9a1d2de435ef6348fb6fb873
Melocalyptic <philip.kim830@gmail.com>
2024-03-04 14:57:16 -0500

    Temp Fixed Cursor for dialogue scenes

--- StuckAtLv1/Assets/Scripts/Systems/UIMouseFollowCamera.cs
+++ StuckAtLv1/Assets/Scripts/Systems/UIMouseFollowCamera.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class UIMouseFollowCamera : MonoBehaviour
 {
-    public Canvas myCanvas;
+    private Canvas myCanvas;
+
     void Start () {
         Cursor.visible = false;
+        myCanvas = GameObject.Find("Canvas").GetComponent<Canvas>();
     }
    
     void Update () {
         Vector2 pos;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, null, out pos);
         transform.position = myCanvas.transform.TransformPoint(pos);
+        Pressed();
+    }
+
+    void Pressed()
+    {
+      if(Input.GetMouseButtonDown(0))
+      {
+        Cursor.visible = false;
+      }
     }
 }
