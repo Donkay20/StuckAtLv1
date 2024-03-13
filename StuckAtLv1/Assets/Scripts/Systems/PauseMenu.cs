8a4cebf3b1537d4069aa845b24116f97439d305f
Donkay20 <supersett@hotmail.com>
2024-03-13 03:14:07 -0400

    v0.6815
    Updates:
    - Backbone for the updated pause menu added.

--- StuckAtLv1/Assets/Scripts/Systems/PauseMenu.cs
+++ StuckAtLv1/Assets/Scripts/Systems/PauseMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private KeyCode initPauseMenu;
     private Animator pauseAnimator;
     private bool pauseMenuOpen;
+    private String status;
 
     void Awake() {
         pauseAnimator = GetComponent<Animator>();
@@ -26,11 +28,33 @@ public class PauseMenu : MonoBehaviour
         pauseAnimator.SetTrigger("Intro");
         Time.timeScale = 0;
         pauseMenuOpen = true;
+        status = "center";
     }
 
     private void PauseMenuEnd() {
         pauseAnimator.SetTrigger("Outro");
         Time.timeScale = 1;
         pauseMenuOpen = false;
+    }
+
+    public void Center() {
+        if (status != "center") {
+            pauseAnimator.SetTrigger("Center");
+            status = "center";
+        }
+    }
+
+    public void Right() {
+        if (status != "right") {
+            pauseAnimator.SetTrigger("Right");
+            status = "right";
+        }
+    }
+
+    public void Left() {
+        if (status != "left") {
+            pauseAnimator.SetTrigger("Left");
+            status = "left";
+        }
     }
 }
