b80dda31614acad4f2f4efa0479a9fd35f8fdf5f
Donkay20 <supersett@hotmail.com>
2024-03-20 03:04:36 -0400

    v0.6931
    Updates:
    - Added Squirrel's third ability, Squirrel Ball, has been implemented.
    
    Adjustments:
    - Added folders for organizational purposes.

---
+++ StuckAtLv1/Assets/Scripts/Attacks/(12-14)Squirrel/Skill 3/SquirrelBall.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SquirrelBall : MonoBehaviour
+{
+    private readonly int SQUIRRELBALL_BASE_DMG = 3;
+    private readonly float SQUIRRELBALL_BASE_DURATION = 3f;
+    Slot slot;
+    CircleCollider2D ballCollider;
+    private float timer;
+    private int damage;
+    private float size;
+    private bool hitCheck;
+    private float hitRefreshRate = 0.3f;
+    private Character character;
+
+    void Start() {
+        slot = GetComponentInParent<Slot>();
+        AttackSlotBonus asb = FindAnyObjectByType<AttackSlotBonus>();
+        ballCollider = GetComponent<CircleCollider2D>();
+        character = FindAnyObjectByType<Character>();
+
+        damage = asb.GetDamageBonus(slot, SQUIRRELBALL_BASE_DMG);
+
+        size = asb.GetSizeBonus(slot);
+        transform.localScale = new Vector2(size, size);
+
+        timer = asb.GetDurationBonus(slot, SQUIRRELBALL_BASE_DURATION);
+
+        character.GainAfterimage(SQUIRRELBALL_BASE_DURATION / 2, false);
+    }
+
+    void Update() {
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            Destroy(gameObject);
+        }
+
+        if (hitRefreshRate > 0 && !hitCheck) {
+            hitRefreshRate -= Time.deltaTime;
+        }
+
+        if (hitRefreshRate <= 0 && !hitCheck) {
+            hitCheck = true;
+        }
+    }
+
+    private void ResetHitCheck() {
+        hitCheck = false;
+        hitRefreshRate = 0.3f;
+    }
+
+    private void OnTriggerStay2D(Collider2D col) {
+        if (hitCheck) {
+            Collider2D[] enemyColliders = Physics2D.OverlapCircleAll(transform.position, ballCollider.radius, LayerMask.GetMask("Enemy"));
+            Collider2D[] passThroughEnemyColliders = Physics2D.OverlapCircleAll(transform.position, ballCollider.radius, LayerMask.GetMask("PassThroughEnemy"));
+            
+            foreach (Collider2D c in enemyColliders.Concat(passThroughEnemyColliders)) {
+                Enemy enemy = c.GetComponent<Enemy>();
+                FindAnyObjectByType<OnHitBonus>().ApplyDamageBonus(slot, enemy, damage);
+            }
+
+            ResetHitCheck();
+        }
+    }
+}
