7f94dd22cf906e3dfd78986301b2c3bd6ce8826a
Donkay20 <supersett@hotmail.com>
2024-04-16 02:02:11 -0400

    v0.73
    Updates:
    - Catacombs mini-boss fully implemented.

--- StuckAtLv1/Assets/Scripts/Enemies/(W3) Catacombs/BigSlime/BigSlime.cs
+++ StuckAtLv1/Assets/Scripts/Enemies/(W3) Catacombs/BigSlime/BigSlime.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,11 +13,22 @@ public class BigSlime : MonoBehaviour
     [SerializeField] private GameObject sizeBar;
     [SerializeField] private Image bossHPBarFill, sizeBarFill;
     [SerializeField] private TextMeshProUGUI bossName, secondaryBarTextName;
-    private readonly float MAX_SIZE = 5;
-    private readonly float ATTACK_TIMER_COOLDOWN = 7f;
+    [Header("Attacks")]
+    [SerializeField] private GameObject slimeBarrageBullet;
+    [SerializeField] private GameObject slimeSlice;
+    private readonly float MAX_SIZE = 4;
+    private readonly float MIN_SIZE = 0.5f;
+    private readonly float ATTACK_TIMER_COOLDOWN = 10f;
     private int goopMaxHp;
     private float size;
     private float attackTimer;
+
+    /*
+        When the boss takes damage from normal attacks, it increases in size, to a maximum of 5x.
+        Getting electrified causes the boss to shrink, to a minimum of 0.5x.
+
+        All of the boss' attacks scale with its size.
+    */
     
     void Start() {
         size = 1;
@@ -48,19 +60,45 @@ public class BigSlime : MonoBehaviour
     }
 
     private void Attack() {
-        //todo
+        int i = Random.Range(0,2);
+        switch (i) {
+            case 0:
+                StartCoroutine(SlimeBarrage());
+                break;
+            case 1:
+                Instantiate(slimeSlice, transform.position, Quaternion.identity, transform);
+                break;
+        }
+        attackTimer = ATTACK_TIMER_COOLDOWN;
+    }
+
+    private IEnumerator SlimeBarrage() {
+        int bulletAmount;
+        if (enemyScript.GetHealth() < (goopMaxHp / 2)) {
+            bulletAmount = 5;
+        } else {
+            bulletAmount = 3;
+        }
+        while (bulletAmount > 0) {
+            Vector2 trajectory = target.transform.position - transform.position;
+            GameObject b = Instantiate(slimeBarrageBullet, transform.position, Quaternion.identity);
+            b.GetComponent<Rigidbody2D>().velocity = trajectory;
+            b.transform.localScale = new Vector2(size, size);
+            bulletAmount--;
+            yield return new WaitForSeconds(1);
+        }
     }
 
     private void TookDamage() {     //whenever the boss takes damage, it increases in size
-        if (size < 5) {
-            size += 0.05f;
+        if (size < MAX_SIZE) {
+            size += 0.025f;
             transform.localScale = new Vector2(size, size);
         }
     }
 
     public void Electrified() {     //whenever the boss is hit by a conductor bolt, its size is reduced
-        if (size <= 0.5f) {
-            size = 0.5f;
+        if (size - 0.05f <= MIN_SIZE) {
+            size = MIN_SIZE;
         } else {
             size -= 0.05f;
         }
---
+++ StuckAtLv1/Assets/Scripts/Enemies/(W3) Catacombs/BigSlime/Conductor/ConductionManager.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ConductionManager : MonoBehaviour
+{
+    [SerializeField] private Conductor[] conductors;
+    [SerializeField] private ConductorBolt[] conductorBolts;
+    [SerializeField] private TextMeshProUGUI conductorCountUI;
+    [SerializeField] private GameObject additionalInfo;
+    private int[] conductorsActivated = new int[3];
+    private ConductorBolt[] chosenBolts = new ConductorBolt[3];
+    private int conductorsActive;
+    private bool boltBarrage;
+
+    /*
+    This class is responsible for the lightning conductor mechanic for the catacombs miniboss, the Goop (bigslime).
+    An additional info field was added to the UI to inform the user of how many conductors are still needed to activate the lightning attack that'll damage the boss.
+
+    Grabbing the boss' ability and firing it at a conductor will charge it, and charging three of them will launch an electric attack in a triangle that'll damage the boss.
+    This attack shrinks the boss, and can only hurt the Goop.
+    */
+
+    void OnEnable() {
+        additionalInfo.SetActive(true);
+        conductorCountUI.text = "Conductors: "+ conductorsActive + "/3";
+    }
+
+    public void ActivateConductor(int id) {
+        conductorsActivated[conductorsActive] = id;
+        conductorsActive++;
+        conductorCountUI.text = "Conductors: "+ conductorsActive + "/3";
+        if (conductorsActive == 3) {
+            BoltBarrageSetup();
+        }
+    }
+
+    private void BoltBarrageSetup() {
+        boltBarrage = true;
+        for (int i = 0; i < 3; i++) {
+            switch(i) {
+                case 0:
+                    switch (conductorsActivated[0], conductorsActivated[1]) {
+                        case (1,2):
+                        case (2,1):
+                            chosenBolts[0] = conductorBolts[0];
+                            break;
+                        case (1,3):
+                        case (3,1):
+                            chosenBolts[0] = conductorBolts[1];
+                            break;
+                        case (1,4):
+                        case (4,1):
+                            chosenBolts[0] = conductorBolts[2];
+                            break;
+                        case (2,3):
+                        case (3,2):
+                            chosenBolts[0] = conductorBolts[3];
+                            break;
+                        case (2,4):
+                        case (4,2):
+                            chosenBolts[0] = conductorBolts[4];
+                            break;
+                        case (3,4):
+                        case (4,3):
+                            chosenBolts[0] = conductorBolts[5];
+                            break;
+                    }
+                    break;
+                case 1:
+                    switch (conductorsActivated[1], conductorsActivated[2]) {
+                        case (1,2):
+                        case (2,1):
+                            chosenBolts[1] = conductorBolts[0];
+                            break;
+                        case (1,3):
+                        case (3,1):
+                            chosenBolts[1] = conductorBolts[1];
+                            break;
+                        case (1,4):
+                        case (4,1):
+                            chosenBolts[1] = conductorBolts[2];
+                            break;
+                        case (2,3):
+                        case (3,2):
+                            chosenBolts[1] = conductorBolts[3];
+                            break;
+                        case (2,4):
+                        case (4,2):
+                            chosenBolts[1] = conductorBolts[4];
+                            break;
+                        case (3,4):
+                        case (4,3):
+                            chosenBolts[1] = conductorBolts[5];
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch (conductorsActivated[2], conductorsActivated[0]) {
+                        case (1,2):
+                        case (2,1):
+                            chosenBolts[2] = conductorBolts[0];
+                            break;
+                        case (1,3):
+                        case (3,1):
+                            chosenBolts[2] = conductorBolts[1];
+                            break;
+                        case (1,4):
+                        case (4,1):
+                            chosenBolts[2] = conductorBolts[2];
+                            break;
+                        case (2,3):
+                        case (3,2):
+                            chosenBolts[2] = conductorBolts[3];
+                            break;
+                        case (2,4):
+                        case (4,2):
+                            chosenBolts[2] = conductorBolts[4];
+                            break;
+                        case (3,4):
+                        case (4,3):
+                            chosenBolts[2] = conductorBolts[5];
+                            break;
+                    }
+                    break;
+            }
+        }
+        StartCoroutine(BoltBarrage());
+    }
+
+    private IEnumerator BoltBarrage() {
+        chosenBolts[0].gameObject.SetActive(true);
+        chosenBolts[1].gameObject.SetActive(true);
+        chosenBolts[2].gameObject.SetActive(true);
+        yield return new WaitForSeconds(5);
+        DeactivateAllConductors();
+    }
+
+    private void DeactivateAllConductors() {
+        conductors[conductorsActivated[0] - 1].Deactivate();
+        conductors[conductorsActivated[1] - 1].Deactivate();
+        conductors[conductorsActivated[2] - 1].Deactivate();
+
+        chosenBolts[0].gameObject.SetActive(false);
+        chosenBolts[1].gameObject.SetActive(false);
+        chosenBolts[2].gameObject.SetActive(false);
+
+        conductorsActivated[0] = 0; conductorsActivated[1] = 0; conductorsActivated[2] = 0;
+        chosenBolts[0] = null; chosenBolts[1] = null; chosenBolts[2] = null;
+
+        conductorsActive = 0; 
+        conductorCountUI.text = "Conductors: "+ conductorsActive + "/3";
+        boltBarrage = false;
+    }
+
+    public bool BarrageActive() {
+        return boltBarrage;
+    }
+}
