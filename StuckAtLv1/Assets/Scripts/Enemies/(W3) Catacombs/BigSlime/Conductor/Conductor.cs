aab446f22447ac7fc6c10704264ebb33d12da07a
Melocalyptic <143928343+Melocalyptic@users.noreply.github.com>
2024-04-18 16:49:29 -0500

    Slime conductors

--- StuckAtLv1/Assets/Scripts/Enemies/(W3) Catacombs/BigSlime/Conductor/Conductor.cs
+++ StuckAtLv1/Assets/Scripts/Enemies/(W3) Catacombs/BigSlime/Conductor/Conductor.cs
@@ -8,17 +8,25 @@ public class Conductor : MonoBehaviour
     [SerializeField] private int identity;
     [SerializeField] private Sprite inactive, active;
     private bool isActive;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     public void Activate() { //conductor only activates if it isn't already activated and if a bolt barrage isn't already ongoing.
         if (!isActive && !conductionManager.BarrageActive()) {
-            GetComponent<SpriteRenderer>().sprite = active;
+            //GetComponent<SpriteRenderer>().sprite = active;
             conductionManager.ActivateConductor(identity);
+            anim.SetBool("Active", true);
             isActive = true;
         }
     }
 
     public void Deactivate() {
         GetComponent<SpriteRenderer>().sprite = inactive;
+        anim.SetBool("Active", false);
         isActive = false;
     }
 }
